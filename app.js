6bccc446fe678d90142e6af66812ca06c98ce391
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-06-05 12:45:19 +0700

    upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let f = 0, f0 = 0, f1 = 0;
+
+let fibonacci = "";
+
+let tmp = "";
+let i = 0;
+while(i < 20){
+    if(i == 0){
+        f = 1;
+        f1 = 1;
+        fibonacci += f + " ";
+    }else{
+        f = f0 + f1;
+        f0 = f1;
+        f1= f;
+        fibonacci += f + " ";
+        console.log(i + " = " + f);
+        if(f % 5 == 0){
+            tmp = f;
+            break;
+        }
+        
+    }
+    i++;
+}
+document.write("Sô chia hết cho 5 trong dãy là " + tmp);
